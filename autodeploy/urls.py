c1cdb6a05f6dad24171a641567a174b617d056dc
ylhb <hnhuangyiyang@126.com>
2016-11-09 16:33:04 +0800

    新增部署openstack计算节点功能

--- autodeploy/urls.py
+++ autodeploy/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^app_deploy_html/$', views.app_deploy_html, name='app_deploy_html'),
     url(r'^app_deploy/$', views.app_deploy, name='app_deploy'),
+    url(r'^compute_deploy_html/$', views.compute_deploy_html, name='compute_deploy_html'),
+    url(r'^compute_deploy/$', views.compute_deploy, name='compute_deploy'),
 ]
--- autodeploy/views.py
+++ autodeploy/views.py
@@ -15,3 +15,15 @@ def app_deploy(request):
     state_args = request.GET['app']
     app_deploy_result = api_exec('%s' %(ip_list), '%s' %(exec_module) , arg='%s' %(state_args), arg_num=1)['return'][0]
     return render(request, 'autodeploy/app_deploy.html', {'app_deploy_result': app_deploy_result})
+
+@login_required
+def compute_deploy_html(request):
+    return render(request, 'autodeploy/compute_deploy.html')
+
+@login_required
+def compute_deploy(request):
+    ip_list = request.GET['ip_list']
+    exec_module = "state.sls"
+    script_args = request.GET['compute']
+    compute_deploy_result = api_exec('%s' %(ip_list), '%s' %(exec_module) , arg='%s' %(script_args), arg_num=1)['return'][0]
+    return render(request, 'autodeploy/compute_deploy.html', {'compute_deploy_result': compute_deploy_result})
